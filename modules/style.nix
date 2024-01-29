898dc2108f340ca1436daebee7a08245bb55217b
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-01-29 11:50:48 -0600

    fix alpha config

--- modules/style.nix
+++ modules/style.nix
@@ -65,41 +65,51 @@
         val = [
           {
             on_press.__raw = "vim.cmd('ene')";
+            opts.position = "center";
             opts.shortcut = "e";
+            opts.width = 22;
             opts.keymap.__raw =
-              ''{"n", "e", "vim.cmd('ene')", {noremap=true}}'';
+              ''{"n", "e", "<cmd>lua vim.cmd('ene')<cr>", {noremap=true}}'';
             type = "button";
             val = "  > New file";
           }
           {
             on_press.__raw = "require('telescope.builtin').find_files";
+            opts.position = "center";
             opts.shortcut = "f";
+            opts.width = 22;
             opts.keymap.__raw = ''
-              {"n", "f", "require('telescope.builtin').find_files", {noremap=true}}'';
+              {"n", "f", "<cmd>lua require('telescope.builtin').find_files()<cr>", {noremap=true}}'';
             type = "button";
             val = "󰭎  > Find file";
           }
           {
             on_press.__raw = "require('telescope.builtin').live_grep";
+            opts.position = "center";
             opts.shortcut = "g";
+            opts.width = 22;
             opts.keymap.__raw = ''
-              {"n", "g", "require('telescope.builtin').live_grep", {noremap=true}}'';
+              {"n", "g", "<cmd>lua require('telescope.builtin').live_grep()<cr>", {noremap=true}}'';
             type = "button";
             val = "󰭎  > Find Text (grep)";
           }
           {
             on_press.__raw = "require('telescope.builtin').oldfiles";
+            opts.position = "center";
             opts.shortcut = "r";
+            opts.width = 22;
             opts.keymap.__raw = ''
-              {"n", "r", "require('telescope.builtin').oldfiles", {noremap=true}}'';
+              {"n", "r", "<cmd>lua require('telescope.builtin').oldfiles()<cr>", {noremap=true}}'';
             type = "button";
             val = "󰭎  > Recent";
           }
           {
             on_press__raw = "vim.cmd('qa')";
+            opts.position = "center";
             opts.shortcut = "q";
+            opts.width = 22;
             opts.keymap.__raw =
-              ''{"n", "q", "vim.cmd('qa')", {noremap=true}}'';
+              ''{"n", "q", "<cmd>lua vim.cmd('qa')<cr>", {noremap=true}}'';
             type = "button";
             val = "󰩈  > Quit NVIM";
           }
