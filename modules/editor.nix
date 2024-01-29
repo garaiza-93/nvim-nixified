1455f5a77131506c3f8ca3237342edf506bc2ad4
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-01-29 16:23:22 -0600

    disable lspsaga, causes issues with node

--- modules/editor.nix
+++ modules/editor.nix
@@ -11,53 +11,53 @@
     treesitter.enable = true;
     trouble.enable = true;
 
-    lspsaga = {
-      enable = true;
-      lightbulb.virtualText = false;
-      callhierarchy.keys = {
-        close = "c";
-        edit = "<CR>";
-        quit = "q";
-        shuttle = "[";
-        split = "ss";
-        vsplit = "sv";
-        tabe = "T";
-        toggleOrReq = "t";
-      };
-      definition.keys = {
-        close = "c";
-        edit = "<CR>";
-        quit = "q";
-        split = "ss";
-        vsplit = "sv";
-        tabe = "T";
-      };
-      diagnostic.keys = {
-        execAction = "e";
-        quit = "q";
-        quitInShow = "q<ESC>";
-        toggleOrJump = "<CR>";
-      };
-      finder.keys = {
-        close = "c";
-        quit = "q";
-        shuttle = "[";
-        split = "ss";
-        tabnew = "T";
-        toggleOrOpen = "<CR>";
-        vsplit = "sv";
-      };
-      outline.keys = {
-        jump = "<CR>";
-        quit = "q";
-        toggleOrJump = "o";
-      };
-      rename.keys = {
-        exec = "<CR>";
-        quit = "q";
-        select = "s";
-      };
-    };
+      # lspsaga = {
+      # enable = true;
+      #  lightbulb.virtualText = false;
+      #  callhierarchy.keys = {
+      #    close = "c";
+      #    edit = "<CR>";
+      #    quit = "q";
+      #    shuttle = "[";
+      #    split = "ss";
+      #    vsplit = "sv";
+      #    tabe = "T";
+      #    toggleOrReq = "t";
+      #  };
+      #  definition.keys = {
+      #    close = "c";
+      #    edit = "<CR>";
+      #    quit = "q";
+      #    split = "ss";
+      #    vsplit = "sv";
+      #    tabe = "T";
+      #  };
+      #  diagnostic.keys = {
+      #    execAction = "e";
+      #    quit = "q";
+      #    quitInShow = "q<ESC>";
+      #    toggleOrJump = "<CR>";
+      #  };
+      #  finder.keys = {
+      #    close = "c";
+      #    quit = "q";
+      #    shuttle = "[";
+      #    split = "ss";
+      #    tabnew = "T";
+      #    toggleOrOpen = "<CR>";
+      #    vsplit = "sv";
+      #  };
+      #  outline.keys = {
+      #    jump = "<CR>";
+      #    quit = "q";
+      #    toggleOrJump = "o";
+      #  };
+      #  rename.keys = {
+      #    exec = "<CR>";
+      #    quit = "q";
+      #    select = "s";
+      #  };
+      # };
 
     nvim-tree = {
       enable = true;
