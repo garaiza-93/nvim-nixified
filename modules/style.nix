c6d74c118601b6f7a307d4a909658f2e989e7cf4
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-01-29 13:22:37 -0600

    refactoring; fix Alpha opening an extra buffer

--- modules/style.nix
+++ modules/style.nix
@@ -1,4 +1,11 @@
-{
+let
+  shortcutOpts = {
+    position = "center";
+    align_shortcut = "right";
+    hl_shortcut = "RainbowDelimiterBlue";
+    width = 40;
+  };
+in {
   colorschemes.tokyonight = {
     enable = true;
     style = "night";
@@ -64,52 +71,52 @@
         type = "group";
         val = [
           {
-            on_press.__raw = "vim.cmd('ene')";
-            opts.position = "center";
-            opts.shortcut = "e";
-            opts.width = 22;
-            opts.keymap.__raw =
-              ''{"n", "e", "<cmd>lua vim.cmd('ene')<cr>", {noremap=true}}'';
+            on_press = "vim.cmd('ene')";
+            opts = {
+              shortcut = "e";
+              keymap.__raw =
+                ''{"n", "e", "<cmd>lua vim.cmd('ene')<cr>", {noremap=true}}'';
+            } // shortcutOpts;
             type = "button";
             val = "  > New file";
           }
           {
             on_press.__raw = "require('telescope.builtin').find_files";
-            opts.position = "center";
-            opts.shortcut = "f";
-            opts.width = 22;
-            opts.keymap.__raw = ''
-              {"n", "f", "<cmd>lua require('telescope.builtin').find_files()<cr>", {noremap=true}}'';
+            opts = {
+              shortcut = "f";
+              keymap.__raw = ''
+                {"n", "f", "<cmd>lua require('telescope.builtin').find_files()<cr>", {noremap=true}}'';
+            } // shortcutOpts;
             type = "button";
             val = "󰭎  > Find file";
           }
           {
             on_press.__raw = "require('telescope.builtin').live_grep";
-            opts.position = "center";
-            opts.shortcut = "g";
-            opts.width = 22;
-            opts.keymap.__raw = ''
-              {"n", "g", "<cmd>lua require('telescope.builtin').live_grep()<cr>", {noremap=true}}'';
+            opts = {
+              shortcut = "g";
+              keymap.__raw = ''
+                {"n", "g", "<cmd>lua require('telescope.builtin').live_grep()<cr>", {noremap=true}}'';
+            } // shortcutOpts;
             type = "button";
             val = "󰭎  > Find Text (grep)";
           }
           {
             on_press.__raw = "require('telescope.builtin').oldfiles";
-            opts.position = "center";
-            opts.shortcut = "r";
-            opts.width = 22;
-            opts.keymap.__raw = ''
-              {"n", "r", "<cmd>lua require('telescope.builtin').oldfiles()<cr>", {noremap=true}}'';
+            opts = {
+              shortcut = "r";
+              keymap.__raw = ''
+                {"n", "r", "<cmd>lua require('telescope.builtin').oldfiles()<cr>", {noremap=true}}'';
+            } // shortcutOpts;
             type = "button";
             val = "󰭎  > Recent";
           }
           {
-            on_press__raw = "vim.cmd('qa')";
-            opts.position = "center";
-            opts.shortcut = "q";
-            opts.width = 22;
-            opts.keymap.__raw =
-              ''{"n", "q", "<cmd>lua vim.cmd('qa')<cr>", {noremap=true}}'';
+            on_press = "vim.cmd('qa')";
+            opts = {
+              shortcut = "q";
+              keymap.__raw =
+                ''{"n", "q", "<cmd>lua vim.cmd('qa')<cr>", {noremap=true}}'';
+            } // shortcutOpts;
             type = "button";
             val = "󰩈  > Quit NVIM";
           }
