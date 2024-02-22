cf0e5006a59714d1125d456eb1bda381e02cbb16
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-02-21 19:03:54 -0600

    cleanup

--- configs/rust-config.nix
+++ configs/rust-config.nix
@@ -1,15 +1,8 @@
 {
   # Import all your configuration modules here
   imports = [
-    ../modules/autocmd.nix
-    ../modules/cmp.nix
     ../modules/dap/rust.nix
-    ../modules/editor.nix
-    ../modules/keybinds.nix
     ../modules/lsp/rust.nix
-    ../modules/options.nix
-    ../modules/style.nix
-    ../modules/ui.nix
     ../modules/which-key/rust.nix
     ./default-config.nix
   ];
