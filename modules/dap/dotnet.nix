f3b14fc87ac4719370079e1dc7a3ae3c3ff70a53
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-02-29 15:25:56 -0600

    add onto dotnet dap config

--- modules/dap/dotnet.nix
+++ modules/dap/dotnet.nix
@@ -16,7 +16,24 @@
         request = "launch";
         program.__raw = ''
           function()
-            return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+            local dllPath = vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/src/bin/Debug/net', 'file')
+            
+            local gitRootCmd = 'git rev-parse --show-toplevel 2>/dev/null'
+            local gitRoot = vim.fn.system(gitRootCmd):gsub("\n", "")
+
+            if gitRoot == "" then
+                return 'Not inside a Git repository.'
+            end
+
+            local debugFilePath = gitRoot .. '/.debug'
+            if vim.fn.filereadable(debugFilePath) == 1 then
+                local debugFileContents = vim.fn.readfile(debugFilePath)
+                return table.concat(debugFileContents, "\n")
+            elseif dllPath ~= "" then
+                return dllPath
+            else
+                return 'No DLL path provided.'
+            end
           end
         '';
       }];
