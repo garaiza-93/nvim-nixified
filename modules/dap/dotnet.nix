d6286bcd1f6a28a4d84aeca24702c0272c854ad1
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-03-01 17:55:47 -0600

    add neotest

--- modules/dap/dotnet.nix
+++ modules/dap/dotnet.nix
@@ -2,42 +2,20 @@
   imports = [ ./common.nix ];
   extraPackages = with pkgs; [ netcoredbg ];
   plugins.dap = {
-    adapters = {
-      executables = {
-        coreclr = {
-          command = "${pkgs.netcoredbg}/bin/netcoredbg";
-          args = [ "--interpreter=vscode" ];
-        };
+    adapters.executables = {
+      coreclr = {
+        command = "${pkgs.netcoredbg}/bin/netcoredbg";
+        args = [ "--interpreter=vscode" ];
       };
     };
-    # configurations = {
-    #   cs = [{
-    #     type = "coreclr";
-    #     name = "launch netcoredbg";
-    #     request = "launch";
-    #     program.__raw = ''
-    #       function()
-    #         local dllPath = vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/src/bin/Debug/net', 'file')
-    #         
-    #         local gitRootCmd = 'git rev-parse --show-toplevel 2>/dev/null'
-    #         local gitRoot = vim.fn.system(gitRootCmd):gsub("\n", "")
-
-    #         if gitRoot == "" then
-    #             return 'Not inside a Git repository.'
-    #         end
-
-    #         local debugFilePath = gitRoot .. '/.debug'
-    #         if vim.fn.filereadable(debugFilePath) == 1 then
-    #             local debugFileContents = vim.fn.readfile(debugFilePath)
-    #             return table.concat(debugFileContents, "\n")
-    #         elseif dllPath ~= "" then
-    #             return dllPath
-    #         else
-    #             return 'No DLL path provided.'
-    #         end
-    #       end
-    #     '';
-    #   }];
-    # };
   };
+
+  extraPlugins = with pkgs.vimPlugins; [ neotest neotest-dotnet ];
+  extraConfigLua = ''
+    require("neotest").setup({
+      adapters = {
+        require("neotest-dotnet")
+      }
+    })
+  '';
 }
--- modules/dap/rust.nix
+++ modules/dap/rust.nix
@@ -1,4 +1,12 @@
 { pkgs, ... }: {
   imports = [ ./common.nix ];
   extraPackages = with pkgs; [ vscode-extensions.vadimcn.vscode-lldb ];
+  extraPlugins = with pkgs.vimPlugins; [ neotest ];
+  extraConfigLua = ''
+    require("neotest").setup({
+      adapters = {
+        require('rustaceanvim.neotest')
+      }
+    })
+  '';
 }
