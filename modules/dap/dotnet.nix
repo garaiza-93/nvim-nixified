ca19ed3dbcd520667167495ad7ba53e2bd5dd328
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-02-29 21:04:07 -0600

    bring back common; move listeners to common

--- modules/dap/dotnet.nix
+++ modules/dap/dotnet.nix
@@ -1,19 +1,6 @@
 { pkgs, ... }: {
   imports = [ ./common.nix ];
   extraPackages = with pkgs; [ netcoredbg ];
-  extraConfigLua = ''
-    local dap, dapui = require("dap"), require("dapui")
-    require('dap.ext.vscode').load_launchjs(nil, { coreclr = {'cs'} })
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close()
-    end
-  '';
   plugins.dap = {
     adapters = {
       executables = {
