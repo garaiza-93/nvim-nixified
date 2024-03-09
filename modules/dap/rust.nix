793c03e7abcf113100fb1a5a67c8166684605b79
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-03-08 19:29:31 -0600

    add adapter for rust dap

--- modules/dap/rust.nix
+++ modules/dap/rust.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }: {
   imports = [ ./common.nix ];
   extraPackages = with pkgs; [ vscode-extensions.vadimcn.vscode-lldb ];
+  plugins.dap = {
+    adapters.executables = {
+      codelldb = {
+        command = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/bin/codelldb";
+        args = [ "--interpreter=vscode" ];
+      };
+    };
+  };
+
   extraPlugins = with pkgs.vimPlugins; [ neotest ];
   extraConfigLua = ''
     require("neotest").setup({
