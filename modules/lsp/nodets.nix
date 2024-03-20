849db4a158b5b87548d24387624a0543f5e4ffdf
Gustavo Araiza <57430880+garaiza-93@users.noreply.github.com>
2024-03-20 11:35:13 -0500

    remove deprecated

--- modules/lsp/nodets.nix
+++ modules/lsp/nodets.nix
@@ -7,7 +7,6 @@
   plugins.none-ls = {
     enable = true;
     sources = {
-      formatting.eslint.enable = true;
       formatting.prettier.enable = true;
       formatting.prettier.disableTsServerFormatter = true;
     };
